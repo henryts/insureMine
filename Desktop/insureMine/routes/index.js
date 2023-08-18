bbb42eda35b042d51f3bae07360d8643100d5388
henry sathyan <henrysathyan@gmail.com>
2023-08-18 22:49:40 +0530

    inital commit

---
+++ Desktop/insureMine/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+// const userRoutes = require('./userRoutes');
+// const accountRoutes = require('./accountRoutes');
+// const policyRoutes = require('./policyRoutes');
+// // ... import other route files
+
+// router.use('/api', userRoutes);
+// router.use('/api', accountRoutes);
+// router.use('/api', policyRoutes);
+
+
+module.exports = router;
